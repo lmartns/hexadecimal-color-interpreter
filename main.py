b0db5e016db663744f71d090b4a61ad390cade7c
lmartns <leonardomartins578@gmail.com>
2026-06-22 18:55:16 -0300

    chore: migrate from ts to python

---
+++ main.py
@@ -0,0 +1,24 @@
+# 1 - read a enter
+# 2 - valid enter
+# 3 - parse hex > rgb
+# 4 - print color in terminal
+
+
+# import re
+#
+#
+# def scanner(text: str) -> str:
+#     return input(text)
+#
+#
+# def hex_valid(regex: str):
+#     match = re.search(regex, scanner("Text your hex color: "))
+#     if match:
+#         print("Hex is valid")
+#     else:
+#         print("Hex is not valid")
+#
+
+hex_string = "ffffff"
+result = int(hex_string, 16)
+print(result)
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { reader } from "./reader.ts";
-
-reader("Type a hexadecimal color: 0_0");
